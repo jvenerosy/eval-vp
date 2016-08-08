165e312f37633840a642f42add107fb6e159adff
JVenerosy <julien.venerosy@gmail.com>
2016-08-08 10:38:15 +0200

    Remove console.log()

--- private/js/controller/single.js
+++ private/js/controller/single.js
@@ -5,8 +5,6 @@ marvel.controller('heroController', function($scope, heroFactory, $routeParams) 
   $scope.posts = heroFactory.getHero($routeParams.id).then(function(hero) {
     $scope.loading = false;
     $scope.hero = hero;
-
-    console.log(hero);
   }, function(msg){
     alert(msg);
   });
